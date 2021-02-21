b30a53252a84ea125604b4cd1752ca774f126ab9
NEE_TT <Nshmendyuk.ru@gmail.com>
2021-02-21 20:23:02 +0300

    update annotations and logging

--- src/main/java/ru/inside/commands/controller/EchoController.java
+++ src/main/java/ru/inside/commands/controller/EchoController.java
@@ -1,15 +1,22 @@
 package ru.inside.commands.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/check")
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/check")
 @RequiredArgsConstructor
+@Slf4j
 public class EchoController {
 
     @GetMapping("/check")
-    public String check() {
+    public String check(HttpServletRequest requestContext) {
+        log.info("API is checked from {}:{}", requestContext.getRemoteAddr(), requestContext.getRemotePort());
         return "API is working.\n";
     }
 }
--- src/main/java/ru/inside/commands/controller/ScriptController.java
+++ src/main/java/ru/inside/commands/controller/ScriptController.java
@@ -1,18 +1,23 @@
 package ru.inside.commands.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.inside.commands.executor.ScriptExecutor;
 
-@RestController("/scripts")
+@RestController
+@RequestMapping("/scripts")
 @RequiredArgsConstructor
+@Slf4j
 public class ScriptController {
     private final ScriptExecutor scriptExecutor;
 
     @GetMapping("/{scriptName}")
     public String runHlfScript(@PathVariable String scriptName) {
+        log.info("Executing script " + scriptName);
         return scriptExecutor.executeScript(scriptName);
     }
 }
