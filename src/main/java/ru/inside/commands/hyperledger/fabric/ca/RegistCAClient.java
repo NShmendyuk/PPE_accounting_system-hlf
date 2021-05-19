d44a7d4111868632a6667dfc2bd7e31972a64047
NEE_TT <Nshmendyuk.ru@gmail.com>
2021-05-19 14:34:55 +0300

    fixed. delete bean identity for regist ca

--- src/main/java/ru/inside/commands/hyperledger/fabric/ca/RegistCAClient.java
+++ src/main/java/ru/inside/commands/hyperledger/fabric/ca/RegistCAClient.java
@@ -1,19 +1,16 @@
 package ru.inside.commands.hyperledger.fabric.ca;
 
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import ru.inside.commands.hyperledger.fabric.ca.admin.CAAuthAdmin;
 import ru.inside.commands.hyperledger.fabric.ca.user.CAAuthUser;
 
 @Slf4j
-@Component
-@NoArgsConstructor
 public class RegistCAClient {
 
     public static void initializeUsersCA() {
         // enrolls the admin and registers the user
         try {
+            log.info("trying enroll users");
             CAAuthAdmin.main(null);
             log.info("admin enrolled");
             CAAuthUser.main(null);
