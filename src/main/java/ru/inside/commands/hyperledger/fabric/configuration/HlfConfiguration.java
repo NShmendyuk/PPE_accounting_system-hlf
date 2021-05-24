0153096696a171b21673aa268289226f87afd070
NEE_TT <Nshmendyuk.ru@gmail.com>
2021-05-24 14:34:07 +0300

    fixed

--- src/main/java/ru/inside/commands/hyperledger/fabric/configuration/HlfConfiguration.java
+++ src/main/java/ru/inside/commands/hyperledger/fabric/configuration/HlfConfiguration.java
@@ -23,7 +23,7 @@ public class HlfConfiguration {
     @Getter
     private Network network;
 
-    private final String HLF_USER_NAME = "manager-org2-04";
+    private final String HLF_USER_NAME = "manager-org2-06";
     private final String HLF_CHAINCODE_NAME = "ppesmart";
     private final String HLF_CHANNEL_NAME = "mychannel";
 
--- src/main/java/ru/inside/commands/service/helper/PPEConverter.java
+++ src/main/java/ru/inside/commands/service/helper/PPEConverter.java
@@ -1,6 +1,7 @@
 package ru.inside.commands.service.helper;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 import ru.inside.commands.entity.Employee;
 import ru.inside.commands.entity.PPE;
 import ru.inside.commands.entity.enums.PPEStatus;
@@ -8,18 +9,24 @@ import ru.inside.commands.hyperledger.entity.PPEContract;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @UtilityClass
+@Slf4j
 public class PPEConverter {
 
     public PPE ppeContractToPPE(PPEContract ppeContract) {
         PPE ppe = new PPE();
-        ppe.setName(ppeContract.getName());
-        ppe.setInventoryNumber(ppeContract.getInventoryNumber());
-        ppe.setLifeTime(Duration.ofDays(ppeContract.getLifeTime()));
-        ppe.setStartUseDate(LocalDateTime.parse(ppeContract.getStartUseDate()));
-        ppe.setPpeStatus(PPEStatus.valueOf(ppeContract.getStatus()));
-        ppe.setPrice(ppeContract.getPrice());
+        try {
+            ppe.setName(ppeContract.getName());
+            ppe.setInventoryNumber(ppeContract.getInventoryNumber());
+            ppe.setLifeTime(Duration.ofDays(ppeContract.getLifeTime()));
+            ppe.setStartUseDate(LocalDateTime.parse(ppeContract.getStartUseDate(), DateTimeFormatter.ofPattern("yyyy-MM-DD'T'HH:mm")));
+            ppe.setPpeStatus(PPEStatus.valueOf(ppeContract.getStatus()));
+            ppe.setPrice(ppeContract.getPrice());
+        } catch (Exception ex) {
+            log.error("Cannot convert contract to ppe");
+        }
         return ppe;
     }
 }
