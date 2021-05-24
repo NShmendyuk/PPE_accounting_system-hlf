497e7a41d729f3ca387167045beb47880fa90f09
NEE_TT <Nshmendyuk.ru@gmail.com>
2021-05-24 17:08:50 +0300

    fixed

--- src/main/java/ru/inside/commands/hyperledger/fabric/configuration/HlfConfiguration.java
+++ src/main/java/ru/inside/commands/hyperledger/fabric/configuration/HlfConfiguration.java
@@ -23,7 +23,7 @@ public class HlfConfiguration {
     @Getter
     private Network network;
 
-    private final String HLF_USER_NAME = "manager-org2-16";
+    private final String HLF_USER_NAME = "manager-org2-17";
     private final String HLF_CHAINCODE_NAME = "ppesmart";
     private final String HLF_CHANNEL_NAME = "mychannel";
 
